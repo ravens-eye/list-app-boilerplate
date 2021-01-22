5bf6429193218da0336986ac22db09504ab5c401
Jon Jackson <ocskier@gmail.com>
2021-01-21 20:43:19 -0500

    feat: add list CRUD request methods

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,4 +4,19 @@ export default {
   testBackEnd: () => {
     return axios.get("/api/test");
   },
+  getAllLists: () => {
+    return axios.get("/api/grocery-lists");
+  },
+  getList: (id) => {
+    return axios.get("/api/grocery-lists/" + id);
+  },
+  addList: (listData) => {
+    return axios.post("/api/grocery-lists", listData);
+  },
+  updateList: ({ id, updateData }) => {
+    return axios.put("/api/grocery-lists/" + id, updateData);
+  },
+  deleteList: (id) => {
+    return axios.get("/api/grocery-lists/" + id);
+  },
 };
